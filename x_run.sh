16315fafeb02604d898076de597d54df6a747c69
gl-coding <cool3stone@gmail.com>
2025-06-24 09:19:00 +0800

    gen pdf to run

--- x_run.sh
+++ x_run.sh
@@ -1,6 +1,6 @@
 rm -rf cropped
 rm -rf split_results
-rm -rf ocr_results
+#rm -rf ocr_results
 
 # 裁剪图片
 python batch_crop.py
@@ -9,4 +9,4 @@ python batch_crop.py
 python image_split.py --batch cropped split_results
 
 # 识别图片
-python batch_ocr.py
+python images_to_pdf.py
