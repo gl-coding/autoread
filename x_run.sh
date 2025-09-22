cefae014dd64c9510e33c5276d4aeda1950e7a93
gl-coding <cool3stone@gmail.com>
2025-09-22 11:13:44 +0800

    fix

--- x_run.sh
+++ x_run.sh
@@ -3,13 +3,13 @@ function gen_pdf() {
     rm -rf split_results
     #rm -rf ocr_results
 
-    # 裁剪图片
+    # 裁剪图片，去掉电脑背景的边框，保留书本的内容
     python batch_crop.py
 
-    # 分割图片
+    # 分割图片，将书本分割为左右两部分
     python image_split.py --batch cropped split_results
 
-    # 识别图片
+    # 识别图片，将左右两部分图片识别为文字，并合并为pdf
     python images_to_pdf.py
 
     mv output/output.pdf ~/Downloads
@@ -18,6 +18,7 @@ function gen_pdf() {
 }
 
 function auto_read(){
+    #先开启微信阅读，打开相应的书，然后运行这个脚本
     python mouse_tracker.py
 }
 
