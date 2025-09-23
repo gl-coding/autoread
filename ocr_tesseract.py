54071e6a61b3a8721312de215553f21d156ba5a6
gl-coding <cool3stone@gmail.com>
2025-09-23 19:07:30 +0800

    ocr_dir

--- ocr_tesseract.py
+++ ocr_tesseract.py
@@ -1,6 +1,23 @@
-import pytesseract
+import pytesseract, os
 from PIL import Image
 
 # 识别图片文字
-text = pytesseract.image_to_string(Image.open('/Users/guolei/Downloads/test2.jpg'), lang='chi_sim')  # 中文需下载语言包
-print(text)
+#text = pytesseract.image_to_string(Image.open('split_results/cropped_screenshot_20250922_202947_right.png'), lang='chi_sim')  # 中文需下载语言包
+#text = pytesseract.image_to_string(Image.open('split_results/cropped_screenshot_20250922_202947_right.png'), lang='eng')  # 中文需下载语言包
+
+def main():
+    #遍历split_results目录下的所有图片，进行OCR识别，并保存到ocr_results目录
+    if os.path.exists('ocr_results'):
+        os.remove('ocr_results')
+    if not os.path.exists('ocr_results'):
+        os.makedirs('ocr_results')
+    for file in os.listdir('split_results'):
+        print(file)
+        if file.endswith('.png'):
+            text = pytesseract.image_to_string(Image.open('split_results/' + file), lang='eng')
+            print(text)
+            with open('ocr_results/' + file.replace('.png', '.txt'), 'w') as f:
+                f.write(text)
+
+if __name__ == "__main__":
+    main()
