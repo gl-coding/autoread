7457d289d98ef9b0415fe2b7b76c19b71512caab
gl-coding <cool3stone@gmail.com>
2025-09-30 22:23:13 +0800

    fix

--- llm_prompt.py
+++ llm_prompt.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-import os,sys,time,multiprocessing
+import os,sys,time,multiprocessing,time
 
 # 先加载环境变量
 load_dotenv()
@@ -103,3 +103,37 @@ class Prompt:
 
         res = completion.choices[0].message.content
         return res
+
+    @staticmethod
+    def trans_segment(topic):
+        completion = client.chat.completions.create(
+            model=os.getenv("DEEPSEEK_MODEL"),
+            messages=[
+                {
+                        "role": "system",
+                        "content": "你是一位英文高级教师，擅长将英文段落、句子、短语、单词翻译成中文，并能给出很好的解释，你也精通语法。我输入一段英文段落，请你严格按照以下要求处理文本：\
+                        1. 任务要求：\
+                            1. 每一个句话都给出翻译，标注“翻译”，原文标注原文\
+                            2. 给出每一句话中涉及的语法知识，标注“语法”\
+                            2. 给出每一句话中所有出现的短语、固定搭配的解释，标注“固定搭配”\
+                            3. 给出每一句话中出现的所有单词的音标和释义，如果有原型给出单词原型，标注“单词”\
+                            4. 务必确保所有的句子、短语、固定搭配、单词都有覆盖\
+                        2. 输入文本： {text}\
+                        ".format(text=topic)
+                },
+                {
+                        "role": "user",
+                        "content": "请帮我翻译“" + topic + "”"
+                }
+            ]
+        )
+
+        res = completion.choices[0].message.content
+        return res
+
+if __name__ == "__main__":
+    st = time.time()
+    prompt = Prompt()
+    res = prompt.trans_segment("INTRODUCTION How This Book Came to Be In the early summer of 2004, I got a phone call from Steve Jobs. He had been scattershot friendly to me over the years, with occasional bursts of intensity, especially when he was launching a new product that he wanted on the cover of Time or featured on CNN, places where I’d worked. But now that I was no longer at either of those places, I hadn’t heard from him much. We talked a bit about the Aspen Institute, which I had recently joined, and I invited him to speak at our summer campus in Colorado. He’d be happy to come, he said, but not to be onstage. He wanted instead to take a walk so that we could talk. That seemed a bit odd. I didn’t yet know that taking a long walk was his preferred way to have a serious conversation. It turned out that he wanted me to write a biography of him. I had recently published one on Benjamin Franklin and was writing one about Albert Einstein, and my initial reaction was to wonder, half jokingly, whether he saw himself as the natural successor in that sequence. Because I assumed that he was still in the middle of an oscillating career that had many more ups and downs left, I demurred. Not now, I said. Maybe in a decade or two, when you retire. I had known him since 1984, when he came to Manhattan to have lunch with Time’s editors and extol his new Macintosh. He was petulant even then, attacking a Time correspondent for having wounded him with a story that was too revealing. But talking to him afterward, I found myself rather captivated, as so many others have been over the years, by his engaging intensity. We stayed in touch, even after he was ousted from Apple. When he had something to pitch, such as a NeXT computer or Pixar movie, the beam of his charm would suddenly refocus on me, and he would take me to a sushi restaurant in Lower Manhattan to tell me that whatever he was touting was the best thing he had ever produced. I liked him. When he was restored to the throne at Apple, we put him on the cover of Time, and soon thereafter he began offering me his ideas for a series we were doing on the most influential people of the century. He had launched his “Think Different” campaign, featuring iconic photos of some of the same people we were considering, and he found the endeavor of assessing historic influence fascinating. After I had deflected his suggestion that I write a biography of him, I heard from him every now and then. At one point I emailed to ask if it was true, as my daughter had told me, that the Apple logo was an homage to Alan Turing, the British computer pioneer who broke the German wartime codes and then committed suicide by biting into a cyanide-laced apple. He replied that he wished he had thought of that, but hadn’t. That started an exchange about the early history of Apple, and I found myself gathering string on the subject, just in case I ever decided to do such a book. When my Einstein biography came out, he came to a book event in Palo Alto and pulled me aside to suggest, again, that he would make a good subject.")
+    print(res)
+    print(f"耗时: {time.time() - st}秒")
---
+++ process_file.py
@@ -0,0 +1,91 @@
+import os
+from llm_prompt import *
+
+MAX_LENGTH = 1500
+#MAX_LENGTH = 200
+#PROMPT = "每一个句话都给出翻译，标注在原文后面括号中，给出这个段落中所有出现的固定搭配和解释，标注“固定搭配”，给出所有单词的音标和释义，标注“单词”"
+PROMPT = ""
+
+file_name_res = "Jobs/book_res.txt"
+if os.path.exists(file_name_res):
+    os.remove(file_name_res)
+
+def append_line_tofile(line, file_name=file_name_res):
+    with open(file_name, 'a') as file:
+        file.write(line + "\n")
+
+def process_file(file_path):
+    res = []
+    max_length, min_length = 0, 10000
+    max_line, min_line = "", ""
+    max_single_line = 0
+    with open(file_path, 'r') as file:
+        content = file.read()
+        for line in content.split("\n"):
+            line = line.strip()
+            if line == "":
+                continue
+            res.append(line)
+
+    print(len(res))
+    for item in res: print(item)
+
+    print("*"*100)
+    local_res = []
+    total_res = []
+    for line in res: 
+        local_res.append(line)
+        if line[-1] == "." or line[-2] == ".":
+            #print(res)
+            total_line = " ".join(local_res)
+            total_res.append(total_line)
+            local_res = []
+    if len(local_res) > 0:
+        total_res.append(" ".join(local_res))
+    print(len(total_res))
+
+    segment_res = []
+    cnt = 0
+    max_length = 0
+    max_line = ""
+    for item in total_res:
+        #print(item)
+        #print("-"*100)
+        segment_res.append(item)
+        cnt += len(item)
+        # if cnt == 3626:
+        #     print(item)
+        #     print(len(item))
+        #     print("-"*100)
+        #     break
+        max_length = max(max_length, cnt)
+        if cnt > MAX_LENGTH:
+            seg_line = " ".join(segment_res)
+            print(seg_line, len(seg_line))
+            append_line_tofile(seg_line + PROMPT)
+            #append_line_tofile(str(len(seg_line)))
+            segment_res = []
+            cnt = 0
+    if len(segment_res) > 0:
+        seg_line = " ".join(segment_res)
+        append_line_tofile(seg_line + PROMPT)
+        #append_line_tofile(str(len(seg_line)))
+        print(seg_line, len(seg_line))
+    print(max_length)
+
+def process_file_with_llm(file_path):
+    prompt = Prompt()
+    with open(file_path, 'r') as file:
+        content = file.read()
+        for line in content.split("\n"):
+            line = line.strip()
+            if line == "":
+                continue
+            res = prompt.trans_segment(line)
+            append_line_tofile(res, "llm_res.txt")
+            break
+
+if __name__ == "__main__":
+    #process_file("book_tmp.txt")
+    process_file("Jobs/book.txt")
+    process_file_with_llm("Jobs/book_res.txt")
